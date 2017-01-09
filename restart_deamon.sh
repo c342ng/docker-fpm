eab2f11934be5eae6d4a2abbd0e3d64e999b3aff
c3t3m3 <c3t3m3@users.noreply.github.com>
2017-01-09 23:49:44 +0800

    Delete restart_deamon.sh

--- restart_deamon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker stop deamon-server && docker rm deamon-server
-docker run -d --name=deamon-server --link=rabbitmq-server:rabbitmq-server --link=mysql-server:mysql-server -e "CC_CONFIG_TYPE=mac" -v /opt/webroot:/usr/share/nginx/html:ro c3t3m3/docker-fpm
